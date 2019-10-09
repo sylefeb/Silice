9063504015912591cf3abfc8cea63e22a6dd33e0
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2019-10-09 22:19:55 +0200

    fixing build for linux and verilator framework

--- tests/verilator/verilator_vga.sh
+++ tests/verilator/verilator_vga.sh
@@ -8,9 +8,9 @@ mkdir $1
 cd $1
 verilator -Wno-PINMISSING -Wno-WIDTH -cc vga.v --profile-cfuncs
 cd obj_dir
-make -f Vvga.mk OPT=-DVL_DEBUG
-make -f Vvga.mk ../../frameworks/verilator/verilator_vga.o verilated.o OPT=-DVL_DEBUG
-g++ ../../frameworks/verilator/verilator_vga.o verilated.o Vvga__ALL.a ../../frameworks/verilator/libverilator_silice.a -o ../../test_$1
+make -f Vvga.mk
+make -f Vvga.mk ../../../../frameworks/verilator/verilator_vga.o verilated.o
+g++ ../../../../frameworks/verilator/verilator_vga.o verilated.o Vvga__ALL.a ../../../../frameworks/verilator/libverilator_silice.a -o ../../test_$1
 cd ..
 cd ..
 
