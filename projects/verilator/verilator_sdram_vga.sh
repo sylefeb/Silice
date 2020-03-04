ecd1ad1c10edb343280998285d8ef8e475b60e1d
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-03-04 17:51:58 +0100

    verilator

--- projects/verilator/verilator_sdram_vga.sh
+++ projects/verilator/verilator_sdram_vga.sh
@@ -10,7 +10,7 @@ verilator -Wno-PINMISSING -Wno-WIDTH -cc vga.v --profile-cfuncs --top-module vga
 cd obj_dir
 make -f Vvga.mk
 make -f Vvga.mk ../../../../frameworks/verilator/verilator_vga.o verilated.o
-g++ ../../../../frameworks/verilator/verilator_vga.o verilated.o Vvga__ALL.a ../../../../frameworks/verilator/libverilator_silice.a -o ../../test_$1
+g++ -O2 ../../../../frameworks/verilator/verilator_vga.o verilated.o Vvga__ALL.a ../../../../frameworks/verilator/libverilator_silice.a -o ../../test_$1
 cd ..
 cd ..
 
