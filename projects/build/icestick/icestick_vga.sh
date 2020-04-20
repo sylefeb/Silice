a6126847aec75f1528345a728c880254a27113c2
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2020-04-20 08:19:33 +0200

    arachne-pnr if next-pnr not available, wip

--- projects/build/icestick/icestick_vga.sh
+++ projects/build/icestick/icestick_vga.sh
@@ -2,13 +2,16 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 export PATH=$PATH:$DIR/../../../tools/fpga-binutils/mingw32/bin/
 
 ../../../bin/silice -f ../../../frameworks/icestick_vga.v $1 -o build.v
-yosys -p 'synth_ice40 -top top -json build.json' build.v
 
 if ! type "nextpnr-ice40" > /dev/null; then
   # try arachne-pnr instead
+  echo "nextpnr not found, trying arachnepnr instead"
+  arachne-pnr -p ../../../frameworks/icestick.pcf build.blif -o build.txt
+  icepack build.txt build.bin
 else
+  yosys -p 'synth_ice40 -top top -json build.json' build.v
   nextpnr-ice40 --hx1k --json build.json --pcf ../../../frameworks/icestick.pcf --asc build.asc
+  icepack build.asc build.bin
 fi
 
-icepack build.asc build.bin
 iceprog build.bin
