b786b506515ca23740f283412aedf0e9baaf1052
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2020-06-30 21:51:27 +0200

    ulx3s build scripts

---
+++ projects/build/ulx3s/blinky.ice
@@ -0,0 +1,8 @@
+algorithm main(output uint8 led)
+{
+  uint28 counter = 0;
+  led := counter[20,8];
+  while (1) {
+    counter = counter + 1;
+  }
+}
---
+++ projects/build/ulx3s/ulx3s_bare.sh
@@ -0,0 +1,17 @@
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export PATH=$PATH:$DIR/../../../bin/:$DIR/../../../tools/fpga-binutils/mingw32/bin/:$DIR/openocd/bin/
+export PYTHONHOME=/mingw32/bin
+export PYTHONPATH=/mingw32/lib/python3.8/
+
+rm build*
+
+silice -f ../../../frameworks/ulx3s.v $1 -o build.v
+
+# exit
+
+yosys -p 'synth_ecp5 -top top -json build.json' build.v
+nextpnr-ecp5 --85k --package CABGA381 --json build.json --textcfg build.config --lpf ulx3s.lpf  -q --timing-allow-fail
+
+ecppack --svf-rowsize 100000 --svf build.svf build.config build.bit
+
+fujprog build.bit
