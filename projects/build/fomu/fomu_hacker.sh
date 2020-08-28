fd99550182403015e4806062fe6ea0c0eba78baa
rob-ng15 <58272847+rob-ng15@users.noreply.github.com>
2020-08-28 12:28:08 +0100

    Corrected files.
    These use a local copy of the pcf and framework file for my testing purposes.

--- projects/build/fomu/fomu_hacker.sh
+++ projects/build/fomu/fomu_hacker.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 rm build*
 
-silice -f ../../../frameworks/fomu.v $1 -o build.v
+silice -f frameworks/fomu.v $1 -o build.v
 
 yosys -D HACKER=1 -p 'synth_ice40 -top top -json build.json' build.v
-nextpnr-ice40 --up5k --package uwg30 --pcf fomu-hacker.pcf --json build.json --asc build.asc
-icepack build.asc build.bin
-cp blink.bit blink.dfu
-dfu-suffix -v 1209 -p 70b1 -a blink.dfu
+nextpnr-ice40 --up5k --package uwg30 --pcf pcf/fomu-hacker.pcf --json build.json --asc build.asc
+icepack build.asc build.bit
+cp build.bit build.dfu
+dfu-suffix -v 1209 -p 70b1 -a build.dfu
