6f2837fa3037cb92b0c9047306efd33fbee0733d
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2021-06-12 16:50:27 +0200

    verilator vga framework cleanup

--- frameworks/verilator/verilator_vga.cpp
+++ frameworks/verilator/verilator_vga.cpp
@@ -18,11 +18,9 @@ the distribution, please refer to it for details.
 #include "Vtop.h"
 
 #include "VgaChip.h"
-#include "sdr_sdram.h"
 
 Vtop    *g_VgaTest = nullptr;
 VgaChip *g_VgaChip = nullptr;
-SDRAM   *g_SDRAM   = nullptr;
 
 unsigned int g_MainTime = 0;
 double sc_time_stamp()
@@ -32,9 +30,6 @@ double sc_time_stamp()
 
 void step()
 {
-  static vluint64_t sdram_dq = 0;  
-  static vluint8_t  prev_vga_vs = 0;
-  
   if (Verilated::gotFinish()) {
     exit (0);
   }
@@ -43,27 +38,6 @@ void step()
 
   g_VgaTest->eval();
 
-  g_SDRAM->eval(g_MainTime,
-            g_VgaTest->sdram_clock, 1,
-            g_VgaTest->sdram_cs,  g_VgaTest->sdram_ras, g_VgaTest->sdram_cas, g_VgaTest->sdram_we,
-            g_VgaTest->sdram_ba,  g_VgaTest->sdram_a,
-            g_VgaTest->sdram_dqm, (vluint64_t)g_VgaTest->sdram_dq_o, sdram_dq);
-
-  g_VgaTest->sdram_dq_i = (g_VgaTest->sdram_dq_en) ? g_VgaTest->sdram_dq_o : sdram_dq;
-
-/*
-  // this is to dump SDRAM in a raw file
-  
-  if (prev_vga_vs == 0 && vga_test->video_vs != 0) {
-    static int cnt = 0;
-    char str[256];
-    snprintf(str,256,"dump_%04d.raw",cnt++);
-    sdr->save(str,4*8192*1024*2,0);
-  }
-*/
-
-  prev_vga_vs = g_VgaTest->video_vs;
-  
   g_VgaChip->eval(
       g_VgaTest->video_clock,
       g_VgaTest->video_vs,g_VgaTest->video_hs,
@@ -87,20 +61,6 @@ int main(int argc,char **argv)
 
   // instantiate VGA chip
   g_VgaChip = new VgaChip((int)g_VgaTest->video_color_depth);
-
-  // instantiate SDRAM
-  vluint8_t sdram_flags = 0;
-  if ((int)g_VgaTest->sdram_word_width == 8) {
-    sdram_flags |= FLAG_DATA_WIDTH_8;
-  } else if ((int)g_VgaTest->sdram_word_width == 16) {
-    sdram_flags |= FLAG_DATA_WIDTH_16;
-  } else if ((int)g_VgaTest->sdram_word_width == 32) {
-    sdram_flags |= FLAG_DATA_WIDTH_32;
-  } else if ((int)g_VgaTest->sdram_word_width == 64) {
-    sdram_flags |= FLAG_DATA_WIDTH_64;
-  }
-  g_SDRAM = new SDRAM(13 /*8192*/, 10 /*1024*/, sdram_flags, NULL); 
-                                                             //"sdram.txt");
 
   while (1) { step(); }
 
