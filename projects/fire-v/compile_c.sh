eb738369b6bac2603bf35d2b6ccb4ad0f2cda7b9
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2021-07-04 19:31:56 +0200

    icebreaker: making oled pmod pinout with icestick ; minor cleanup in compile scripts

--- projects/fire-v/compile_c.sh
+++ projects/fire-v/compile_c.sh
@@ -46,5 +46,5 @@ $ARCH-elf-ld -m elf32lriscv -b elf32-littleriscv -Tsmoke/config_cpu$CPU.ld --no-
 $ARCH-elf-objcopy -O verilog build/code.elf build/code$CPU.hex
 
 # uncomment to see the actual code, usefull for debugging
-$ARCH-elf-objcopy.exe -O binary build/code.elf build/code.bin
-$ARCH-elf-objdump.exe -D -b binary -m riscv build/code.bin 
+# $ARCH-elf-objcopy.exe -O binary build/code.elf build/code.bin
+# $ARCH-elf-objdump.exe -D -b binary -m riscv build/code.bin 
