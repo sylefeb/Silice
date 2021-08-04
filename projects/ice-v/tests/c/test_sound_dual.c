ef9211181f25b84c2242c094cf48ed86066ce071
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2021-08-04 21:32:36 +0200

    audio tests

---
+++ projects/ice-v/tests/c/test_sound_dual.c
@@ -0,0 +1,43 @@
+inline unsigned int time() 
+{
+   unsigned int cycles;
+   asm volatile ("rdcycle %0" : "=r"(cycles));
+   return cycles>>1;
+}
+
+inline unsigned int cpu_id() 
+{
+   unsigned int cycles;
+   asm volatile ("rdcycle %0" : "=r"(cycles));
+   return cycles&1;
+}
+
+void main() 
+{
+  volatile int* const LEDS  = (int*)0x2004;
+  volatile int* const SOUND = (int*)0x2020;
+
+  int s   = 0;
+  int dir = 1;
+
+  unsigned int cy_last = time();
+
+  if (cpu_id() == 0) {
+    while (1) {
+      unsigned int cy = time();
+      if (cy < cy_last) { cy_last = cy; } // counter may wrap around
+      if (cy > cy_last + 1407) {
+        *SOUND  = s;
+        cy_last = cy;
+        s += dir;
+        if (s > 127 || s < -127) {
+          dir = -dir;
+        }
+        *LEDS = cy; 
+      }
+    }
+  } else {
+    while (1) { }
+  }
+  
+}
