95c18c07240b98a907d5c16655b223966406932a
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2021-09-07 09:33:12 +0200

    improving riscv toolchain detection

--- tools/bash/find_riscv.sh
+++ tools/bash/find_riscv.sh
@@ -1,8 +1,8 @@
-if type "riscv64-unknown-elf-gcc" > /dev/null; then
+if type "riscv64-unknown-elf-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-unknown-elf"
-elif type "riscv64-unknown-gnu-gcc" > /dev/null; then
+elif type "riscv64-unknown-gnu-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-unknown-gnu"
-elif type "riscv64-linux-elf-gcc" > /dev/null; then
+elif type "riscv64-linux-elf-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-linux-elf"
 else
   ARCH="riscv64-linux-gnu"
