4f1e6b3c8cf9aaf03ce179133a02775963d665d3
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2021-09-08 10:02:19 +0200

    dual-fractal demo

--- projects/ice-v/src/dual_fractal.c
+++ projects/ice-v/src/dual_fractal.c
@@ -1,17 +1,23 @@
+// ========== Real time Julia fractal on the ice-v-dual
+// mul trick from http://cowlark.com/2018-05-26-bogomandel/index.html
+//
+// CPU0 computes even pixels, CPU1 odd pixels
+// They are synchronized through synch
 
-// fixed point setup
-#define FP     8
+// ==== used to synchronize CPUs
+volatile int synch;
+
+// ==== fixed point setup
+#define FP     6
 #define IP     (FP+3)
 #define CUTOFF (4<<FP)
-
-// to regenerate the squares table do (from projects/ice-v):
-//    cd src ; gcc dual_fractal.c ; ./a.exe ; cd ..
-// this outputs "squares.h"
 
 #ifdef __riscv
 
 #include "oled.h"
+#include "dual_fractal_table.h"
 
+// ==== returns the CPU ID
 static inline int cpu_id() 
 {
    unsigned int cycles;
@@ -19,25 +25,7 @@ static inline int cpu_id()
    return cycles&1;
 }
 
-#include "squares.h"
-
-// #define USE_MUL
-
-#ifdef USE_MUL
-//https://github.com/gcc-mirror/gcc/blob/master/libgcc/config/epiphany/mulsi3.c
-// GPLv3
-unsigned int __mulsi3 (unsigned int a, unsigned int b)
-{
-  unsigned int r = 0;
-  while (a) {
-      if (a & 1) r += b;
-      a >>= 1; b <<= 1;
-    }
-  return r;
-}
-#endif
-
-volatile int synch;
+// ==== main render loop, runs on each core
 
 void main_loop(int who)  
 {
@@ -46,74 +34,38 @@ void main_loop(int who)
     oled_fullscreen();
   }
 
-#ifdef USE_MUL
-
-  int offs = 0;
-  while (who == 0) { // only core 0 will do something
-    // V1 standard fixed point fractal with multiply
-    for (int j = 0;j < 128; ++j) {
-      int j_f = ((j-64)<<FP)>>5;   // y [ -2:2 ]
-      for (int i = 0;i < 128; ++i) {
-        int i_f = ((i-64)<<FP)>>5; // x [ -2:2 ]
-        int x_f = i_f;
-        int y_f = j_f;
-        int it = 0;
-        for (;it<32;++it) {
-          int u_f  = (x_f*x_f)  >>FP;
-          int v_f  = (y_f*y_f)  >>FP;
-          int w_f  = (2*x_f*y_f)>>FP;
-          y_f = w_f + j_f;
-          x_f = u_f - v_f + i_f;
-          if (u_f + v_f > CUTOFF) {
-            break;
-          }
-        }
-        it = (it + offs) & 31;
-        oled_pix_565(0,it&31);
-      }
-    }
-    ++offs;
-  }
-
-#else
-
 #define MASK   ((1<<(IP+1))-1)
 #define CLAMP  ((1<<(IP-1))-1)
 #define NEG    ((1<<(IP+1))  )
 
-  //int x_c = 128;
-	//int y_c = -16;
-  int zm = 16; int zd =-1;
+#define XCmin  -90
+#define XCmax  (XCmin+40)
+#define YCmin  10
+#define YCmax  (YCmin+40)
+
+	int x_c = XCmin; int x_c_i = 1;
+  int y_c = YCmin; int y_c_i = 3;
+
   while (1) {
 
-    int j_f = -128; // - 64*2;
+    int j_f = -64;
     int pix = who;
     for (int j = 0 ; j < 128 ; ++j) {
-      int i_f = -128*4; // - 128*3 + offs;
+      int i_f = -64;
       for (int i = who ; i < 128 ; i += 2/*two cores*/) {
         int x_f  = i_f;
         int y_f  = j_f;
-        int n    = 0;
-        int it_r = 0;
-        int it_b = 0;
-        for ( ; n<16 ; n++,it_r+=2,it_b+=8) {
-#define REMAP(X,A) A = (X&NEG)?-X:X; A = A<=CLAMP?A:CLAMP;
-//          int a_f = x_f & MASK;
-//          int b_f = y_f & MASK;
-          int a_f;
-          REMAP(x_f,a_f);
-          int b_f;
-          REMAP(y_f,b_f);
+        int clr=0; int clr8=16;
+        for ( ; clr<24 ; clr++,clr8+=8) {
+          int a_f = x_f & MASK;
+          int b_f = y_f & MASK;
           int u_f = sq[a_f];
           int v_f = sq[b_f];
-//          int c_f = (x_f+y_f) & MASK;
-          int tmp = (x_f+y_f);
-          int c_f;
-          REMAP(tmp,c_f);
+          int c_f = (x_f+y_f) & MASK;
           int s_f = sq[c_f];
           int w_f = s_f - u_f - v_f;
-          x_f     = u_f - v_f + i_f;//+ x_c; //+ i_f;
-          y_f     = w_f       + j_f;//+ y_c; // + j_f;
+          x_f     = u_f - v_f + x_c; // + i_f; // use for Mandlebrot
+          y_f     = w_f       + y_c; // + j_f; // use for Mandlebrot
           if (u_f + v_f > CUTOFF) {
             break;
           }
@@ -121,23 +73,26 @@ void main_loop(int who)
         // wait on lock
         while (pix > synch) { }
         // send pixel
-        oled_pix_565(it_b,it_r);
+        oled_pix_565(clr8,clr);
         // asm volatile ("nop;");
         // advance two pixels (meanwhile OLED completes)
         pix += 2;
-        i_f += zm + zm;
+        i_f += 2;
         // advance lock
         ++synch;
       }
-      j_f += zm;
+      j_f += 1;
     }
-    zm = zm + zd;
-    if (zm == 1 || zm == 16) { zd = -zd; }
+
+    x_c += x_c_i;
+    if (x_c < XCmin || x_c > XCmax) { x_c_i = - x_c_i; }
+    y_c += y_c_i;
+    if (y_c < YCmin || y_c > YCmax) { y_c_i = - y_c_i; }
     
   }
-#endif
-  
 }
+
+// ==== main calls the render loop for each CPU
 
 void main() 
 {
@@ -145,18 +100,21 @@ void main()
   synch = 0;
 
   if (cpu_id() == 0) {
-
     main_loop(0);
-
   } else {
-
     main_loop(1);
-
   }
 
 }
 
 #else
+
+// =================== this gets used when compiling with gcc one desktop =====
+// =================== recomputes the dual_fractal_table.h file
+
+// to regenerate the squares table do (from projects/ice-v):
+//    cd src ; gcc dual_fractal.c ; ./a.exe ; cd ..
+// this outputs "dual_fractal_table.h"
 
 #include <stdio.h>
 
@@ -165,7 +123,6 @@ void main()
 {
   unsigned short sq[1<<(IP+1)];
 
-  int num_clamped = 0;
   for (int s = 0; s < (1<<(IP+1)) ; ++s) {
     int q = s;
     if (s >= (1<<IP)) {
@@ -175,19 +132,14 @@ void main()
     }
     sq[s]  = (q*q)>>FP;
     if (sq[s] >= (1<<IP)) {
-      if (num_clamped == 0) {
-        printf("first clamped at %d\n",s);
-      }
-      num_clamped ++;
       sq[s] = (1<<IP)-1;
     }
   }
-  printf("num clamped: %d / %d",num_clamped,1<<(IP+1));
 
-  FILE *f = fopen("squares.h","w");
+  FILE *f = fopen("dual_fractal_table.h","w");
   fprintf(f,"unsigned short sq[] = {");
-  for (int s = 0; s < (1<<(IP-1)) ; ++s) {
-  // for (int s = 0; s < (1<<(IP+1)) ; ++s) {
+  // for (int s = 0; s < (1<<(IP-1)) ; ++s) {
+  for (int s = 0; s < (1<<(IP+1)) ; ++s) {
     if (s>0) fprintf(f,",");
     fprintf(f,"%d",sq[s]);
   }
