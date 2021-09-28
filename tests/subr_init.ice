sylefeb/Silice

commit 979b8f861d03690b39fde4a896a25813bbc30a54

fix for #187 (cycle counts in case of ++ after while now correct)

src/Algorithm.cpp
@@ -3397,10 +3397,6 @@ const Algorithm::t_combinational_block *Algorithm::fastForward(const t_combinati
   const t_combinational_block *current = block;
   const t_combinational_block *last_state = block;
   while (true) {
-    if (current->no_skip) {
-      // no skip, stop here
-      return last_state;
-    }
     if (!current->instructions.empty()) {
       bool stop = true;
       if (current->instructions.size() == 1) {
@@ -3436,6 +3432,10 @@ const Algorithm::t_combinational_block *Algorithm::fastForward(const t_combinati
       return last_state;
     } else {
       current = current->next()->next;
+    }
+    if (current->no_skip) {
+      // no skip, stop here
+      return last_state;
     }
     if (current->is_state) {
       last_state = current;

commit c9a47f296805d1cd2e71b852f176b54fa5db55ba

fixing issue introduced with 979b8f861d03690b39fde4a896a25813bbc30a54 where subroutine var init would be skipped during state fastforward

src/Algorithm.cpp
@@ -3395,6 +3395,10 @@ const Algorithm::t_combinational_block *Algorithm::fastForward(const t_combinati
 {
   sl_assert(block->is_state);
   const t_combinational_block *current = block;
+  if (current->no_skip) {
+    // no skip, stop here
+    return current;
+  }
   const t_combinational_block *last_state = block;
   while (true) {
     if (!current->instructions.empty()) {

tests/subr_init.ice
@@ -1,0 +1,49 @@
+algorithm main(output uint8 leds)
+{
+  uint24 cycle(0);
+  uint24 last(0);
+ /*
+  subroutine wait1()
+  {
+    uint24 count = uninitialized;
+    count = 0;
+    while (count != 16) {
+      count = count + 1;
+    }
+  }
+*/
+  subroutine wait2()
+  {
+    uint24 count = 0;
+    while (count != 16) {
+      count = count + 1;
+    }
+  }
+
+  always_after{ cycle = cycle + 1; }
+
+  __display("start %d",cycle);
+  last = cycle;
+  () <- wait2 <- ();
+  __display("elapsed (2) %d",cycle-last);
+  last = cycle;
+  () <- wait2 <- ();
+  __display("elapsed (2) %d",cycle-last);
+  last = cycle;
+  () <- wait2 <- ();
+  __display("elapsed (2) %d",cycle-last);
+  last = cycle;
+/*
+  __display("start %d",cycle);
+  last = cycle;
+  () <- wait1 <- ();
+  __display("elapsed (1) %d",cycle-last);
+  last = cycle;
+  () <- wait1 <- ();
+  __display("elapsed (1) %d",cycle-last);
+  last = cycle;
+  () <- wait1 <- ();
+  __display("elapsed (1) %d",cycle-last);
+  last = cycle;
+*/
+}