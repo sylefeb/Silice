d01f5a55bae45a967b99fbae6abf7704c8ffa842
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2021-10-23 14:16:44 +0200

    spi screen simulation, vertical flip

--- frameworks/verilator/SPIScreen.cpp
+++ frameworks/verilator/SPIScreen.cpp
@@ -204,7 +204,8 @@ void SPIScreen::cmd_write_ram()
     // fprintf(stdout,"666 %d x %d, y %d\n",m_step,m_x_cur,m_y_cur);
     m_rgb[(m_step - 1)] = m_byte;
     if (m_step == 3) {
-      m_framebuffer.pixel<LibSL::Memory::Array::Wrap>(m_y_cur,m_x_cur) = m_rgb;
+      m_framebuffer.pixel<LibSL::Memory::Array::Wrap>(
+                                  m_y_cur,m_framebuffer.h()-1-m_x_cur) = m_rgb;
     }
     m_step = m_step + 1;
     if (m_step > 3) {
@@ -233,7 +234,8 @@ void SPIScreen::cmd_write_ram()
       m_rgb[0] <<= 3;      m_rgb[1] <<= 2;      m_rgb[2] <<= 3;
       //fprintf(stdout,"565 x %d, y %d rgb:%d,%d,%d\n",
       //        m_x_cur,m_y_cur,(int)m_rgb[0],(int)m_rgb[1],(int)m_rgb[2]);
-      m_framebuffer.pixel<LibSL::Memory::Array::Wrap>(m_y_cur,m_x_cur) = m_rgb;
+      m_framebuffer.pixel<LibSL::Memory::Array::Wrap>(
+                                  m_y_cur,m_framebuffer.h()-1-m_x_cur) = m_rgb;
     }
     m_step = m_step + 1;
     if (m_step > 2) {
