fd5fc4e3bd94ecf7c2a5b2442e0f4202beb11a75
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2021-12-24 11:46:28 +0100

    commenting SPIscreen debug msgs

--- frameworks/verilator/SPIScreen.cpp
+++ frameworks/verilator/SPIScreen.cpp
@@ -101,7 +101,7 @@ void SPIScreen::cmd_idle_SSD1351()
 {
   m_step = 0;
   if (!m_dc) {
-    fprintf(stdout,"command: %x\n", m_byte);
+    // fprintf(stdout,"command: %x\n", m_byte);
     switch (m_byte) {
       case 0x15:
         m_command = std::bind( &SPIScreen::cmd_start_end, this, &m_x_start, &m_x_end, 1);
@@ -135,7 +135,7 @@ void SPIScreen::cmd_idle_ST7789_ILI9351()
 {
   m_step = 0;
   if (!m_dc) {
-    fprintf(stdout,"command: %x\n", m_byte);
+    // fprintf(stdout,"command: %x\n", m_byte);
     switch (m_byte) {
       case 0x2A:
         m_command = std::bind( &SPIScreen::cmd_start_end, this, &m_x_start, &m_x_end, 2);
@@ -180,7 +180,7 @@ void SPIScreen::cmd_start_end(int *p_start,int *p_end,int nbytes)
     *p_end    = m_byte << 8;
   } else {
     *p_end   |= m_byte;
-    fprintf(stdout,"start_end: %d => %d\n", *p_start, *p_end);
+    // fprintf(stdout,"start_end: %d => %d\n", *p_start, *p_end);
     set_idle();
   }
   m_step = m_step + 1;
