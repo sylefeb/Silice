50d73a9bb57a57f9d35fd836df603f187e8e4875
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2022-01-03 23:37:05 +0100

    revising the bare framework, minor edits to blinky

--- frameworks/boards/bare/bare.v
+++ frameworks/boards/bare/bare.v
@@ -1,1 +1,60 @@
-$$NUM_LEDS = 4
+/*
+
+Copyright 2019, (C) Sylvain Lefebvre and contributors
+List contributors with: git shortlog -n -s -- <filename>
+
+MIT license
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+(header_2_M)
+
+*/
+`define BARE 1
+
+$$SIMULATION=1
+$$BARE=1
+$$NUM_LEDS=8
+$$config['bram_wenable_width'] = '1'
+$$config['dualport_bram_wenable0_width'] = 'data'
+$$config['dualport_bram_wenable1_width'] = 'data'
+$$config['simple_dualport_bram_wenable0_width'] = 'data'
+$$config['simple_dualport_bram_wenable1_width'] = 'data'
+
+module top(
+  output [7:0] out_leds,
+  input        clock
+);
+
+reg ready = 0;
+
+always @(posedge clock) begin
+  ready <= 1;
+end
+
+wire run_main;
+assign run_main = 1'b1;
+
+M_main __main(
+  .clock(clock),
+  .reset(~ready),
+  .out_leds(out_leds),
+  .in_run(run_main)
+);
+
+endmodule
--- projects/blinky/blinky.ice
+++ projects/blinky/blinky.ice
@@ -4,17 +4,21 @@
 
 algorithm main(output uint$NUM_LEDS$ leds) // $NUM_LEDS$ is replaced by the preprocessor,
 {                                          // e.g. this becomes uint5 on the IceStick and IceBreaker
+
+$$if not SIMULATION then
   uint28 cnt = 0; // 28 bits wide unsigned int
+$$else
+  uint$NUM_LEDS$ cnt = 0;
+$$end
 
   // leds tracks the most significant bits of the counter
   leds := cnt[ widthof(cnt)-widthof(leds) , widthof(leds) ];
 
-$$if SIMULATION then
-  while (cnt < 256) { // if siumlation we perform only 256 cycles
-$$else
   while (1) {         // if hardware, run forever
-$$end
     cnt  = cnt + 1; // increase cnt (loops back to zero after overflow)
+$$if SIMULATION and not BARE then
+		if (cnt == 64) { __finish(); }
+$$end
   }
 
 }
