910e0de032659f3b7ba123dd14e98d7b86b13212
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2022-02-17 23:29:36 +0100

    riscv toolchain detection script, additional case

--- tools/bash/find_riscv.sh
+++ tools/bash/find_riscv.sh
@@ -4,6 +4,8 @@ elif type "riscv64-unknown-gnu-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-unknown-gnu"
 elif type "riscv64-linux-elf-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-linux-elf"
+elif type "riscv64-elf-gcc" > /dev/null 2>&1; then
+  ARCH="riscv64-elf"
 else
   ARCH="riscv64-linux-gnu"
 fi
