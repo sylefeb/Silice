263001d76b61aa80014426e853c916eeb1cdd0e7
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2022-06-01 16:33:55 +0200

    expanded riscv detection from bash

--- tools/bash/riscv_arch.sh
+++ tools/bash/riscv_arch.sh
@@ -6,6 +6,14 @@ elif type "riscv64-linux-elf-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-linux-elf"
 elif type "riscv64-elf-gcc" > /dev/null 2>&1; then
   ARCH="riscv64-elf"
+elif type "riscv32-unknown-elf-gcc" > /dev/null 2>&1; then
+  ARCH="riscv32-unknown-elf"
+elif type "riscv32-unknown-gnu-gcc" > /dev/null 2>&1; then
+  ARCH="riscv32-unknown-gnu"
+elif type "riscv32-linux-elf-gcc" > /dev/null 2>&1; then
+  ARCH="riscv32-linux-elf"
+elif type "riscv32-elf-gcc" > /dev/null 2>&1; then
+  ARCH="riscv32-elf"
 else
   ARCH="riscv64-linux-gnu"
 fi
