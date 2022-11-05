sylefeb/Silice

commit 8b93790b727d6ea8e63042cda95deae35c33e4b4

pipeline fsms, fixed issue on stage outputs, fixed issue with join of calls not being taken into account in determineVIOAccess (warning: contradicts commit 763c5736d)

src/Algorithm.cpp
@@ -3627,7 +3627,7 @@ void Algorithm::generateStates(t_fsm_nfo *fsm)
   fsm->maxState++;
   // report
   std::cerr << "algorithm " << m_Name
-    << " fsm " << (int64_t)fsm
+    << " fsm " << sprint("%x",(int64_t)fsm)
     << " num states: " << fsm->maxState;
   if (hasNoFSM()) {
     std::cerr << " (no FSM)";
@@ -4560,10 +4560,10 @@ void Algorithm::determineVIOAccess(
         // calling a blueprint?
         auto B = m_InstancedBlueprints.find(sync->joinExec()->IDENTIFIER()->getText());
         if (B != m_InstancedBlueprints.end()) {
-          // if params are empty we skip, otherwise we mark the input as written
+          // if params are empty we skip, otherwise we mark the inputs as written
           auto plist = sync->callParamList();
-          if (!plist->expression_0().empty()) {
-            // inputs
+          if (!plist->expression_0().empty() && !B->second.blueprint.isNull()) {
+            // inputs                            ^^^^^^^^^ this happens when determineVIOAccess is called during gather  TODO FIXME
             for (const auto& i : B->second.blueprint->inputs()) {
               string var = B->second.instance_prefix + "_" + i.name;
               if (vios.find(var) != vios.end()) {
@@ -4582,16 +4582,8 @@ void Algorithm::determineVIOAccess(
             }
           }
         }
-        // do not blindly recurse otherwise the child 'join' is reached
-        recurse = false;
-        // detect reads on parameters
-        for (auto c : node->children) {
-          if (dynamic_cast<siliceParser::JoinExecContext*>(c) != nullptr) {
-            // skip join, taken into account in return block
-            continue;
-          }
-          determineVIOAccess(c, vios, bctx, _read, _written);
-        }
+        // recurse
+        recurse = true;
       }
     } {
       auto async = dynamic_cast<siliceParser::AsyncExecContext*>(node);
@@ -4601,8 +4593,8 @@ void Algorithm::determineVIOAccess(
         if (B != m_InstancedBlueprints.end()) {
           // if params are empty we skip, otherwise we mark the input as written
           auto plist = async->callParamList();
-          if (!plist->expression_0().empty()) {
-            // inputs
+          if (!plist->expression_0().empty() && !B->second.blueprint.isNull()) {
+            // inputs                            ^^^^^^^^^ this happens when determineVIOAccess is called during gather  TODO FIXME
             for (const auto& i : B->second.blueprint->inputs()) {
               string var = B->second.instance_prefix + "_" + i.name;
               if (vios.find(var) != vios.end()) {
@@ -5986,10 +5978,10 @@ void Algorithm::writeAlgorithmReadback(antlr4::tree::ParseTree *node, std::strin
       a.instance_name.c_str());
   }
   // check for pipeline
-  if (bctx->pipeline_stage != nullptr) {
+  /*if (bctx->pipeline_stage != nullptr) {
     reportError(sourceloc(node),
       "cannot join algorithm instance from a pipeline");
-  }
+  }*/
   // check for clock domain crossing
   if (a.instance_clock != m_Clock) {
     reportError(sourceloc(node),
@@ -6040,6 +6032,7 @@ void Algorithm::writeSubroutineCall(antlr4::tree::ParseTree *node, std::string p
   if (bctx->pipeline_stage != nullptr) {
     reportError(sourceloc(node),
       "cannot call a subroutine from a pipeline");
+    // NOTE: why? because the subroutine belongs to the root fsm, not cannot jump to/from a pipeline fsm
   }
   // parse parameters
   std::vector<t_call_param> matches;
@@ -6062,10 +6055,10 @@ void Algorithm::writeSubroutineCall(antlr4::tree::ParseTree *node, std::string p
 
 void Algorithm::writeSubroutineReadback(antlr4::tree::ParseTree *node, std::string prefix, std::ostream& out, const t_subroutine_nfo* called, const t_combinational_block_context* bctx, const t_instantiation_context &ictx, siliceParser::CallParamListContext* plist, t_vio_ff_usage &_ff_usage) const
 {
-  if (bctx->pipeline_stage != nullptr) {
+  /*if (bctx->pipeline_stage != nullptr) {
     reportError(sourceloc(node),
     "cannot join a subroutine from a pipeline");
-  }
+  }*/
   // parse parameters
   std::vector<t_call_param> matches;
   parseCallParams(plist, called, false, bctx, matches);
@@ -7736,6 +7729,11 @@ const Algorithm::t_combinational_block *Algorithm::writeStatelessPipeline(
         out << rewriteIdentifier(prefix, tv.first, "", &st.first->context, ictx, t_source_loc(), FF_D, true, deps, _ff_usage);
         out << ';' << nxl;
         if (!fsmIsEmpty(fsm)) {
+          out << "end else begin" << nxl;
+          out << rewriteIdentifier(prefix, tricklingdst, "", &st.first->context, ictx, t_source_loc(), FF_D, true, deps, _ff_usage);
+          out << " = ";
+          out << rewriteIdentifier(prefix, tricklingdst, "", &st.first->context, ictx, t_source_loc(), FF_Q, true, deps, _ff_usage);
+          out << ';' << nxl;
           out << "end" << nxl;
         }
       } else if (stage < tv.second[1]) {

tests/alg0.si
@@ -5,13 +5,13 @@ algorithm alg(input uint8 num,output uint8 ret)
 }
 
 algorithm main(
-  output int8 led 
+  output int8 leds
 )
 {
   uint8 val = 231;
 
   alg alg0;
 
-  (led) <- alg0 <- (val);
+  (leds) <- alg0 <- (val);
 }
 

tests/globsub1.si
@@ -4,7 +4,7 @@ subroutine inc(input uint8 i,output uint8 o)
   return;
 }
 
-algorithm main()
+algorithm main(output uint8 leds)
 {
   uint8 a = 0;
 

tests/subcall2.si
@@ -1,8 +1,9 @@
 algorithm main( 
-  input  uint1 btn,
-  output uint8 led
+  output uint8 leds
   )
 {
+
+  uint1 btn(0);
 
   subroutine inc(
     input  uint8 a,
@@ -34,5 +35,5 @@ algorithm main(
   {
     v = v + 1;
   }
-  led = v;
+  leds = v;
 }

commit 3ae548e83379db1c5037741fd5956b2bfce99f11

improved unit tests, fixed issue introduced in 8b93790b7 (reverting sync/join determineVIOAccess to 763c5736d)

frameworks/boards/verilator/verilator.sh
@@ -52,11 +52,6 @@ else
 echo "VERILATOR_ROOT already defined, using its value"
 fi
 
-if [[ ! -z "${NO_BUILD}" ]]; then
-  echo "Skipping build."
-  exit
-fi
-
 # check Verilator support for -Wno-TIMESCALEMOD
 set +e
 RET=$((verilator -Wno-TIMESCALEMOD) 2>&1)
@@ -74,6 +69,11 @@ rm -rf $BUILD_DIR/obj_dir
 cd $BUILD_DIR
 
 silice --frameworks_dir $FRAMEWORKS_DIR -f $FRAMEWORK_FILE -o build.v $1 "${@:2}"
+
+if [[ ! -z "${NO_BUILD}" ]]; then
+  echo "Skipping build."
+  exit
+fi
 
 LIBSL_DIR=$SILICE_DIR/../src/libs/LibSL-small/src/LibSL/
 VERILATOR_LIB_DIR=$SILICE_DIR/../frameworks/verilator/

frameworks/libraries/riscv/ice-v-dual/riscv-soc.si
@@ -45,9 +45,9 @@ $$end
 
   // io mapping
   always {
-    uint1 wen      <:  (memio.wenable != 0);
-    uint1 io_write <:  (memio.addr[$external$,1]    &  wen);
-    uint1 io_read  <:: (prev_mem_addr[$external$,1] & ~prev_mem_rw);
+    uint1 wen      = (memio.wenable != 0);
+    uint1 io_write = (memio.addr[$external$,1]    &  wen);
+    uint1 io_read  = (prev_mem_addr[$external$,1] & ~prev_mem_rw);
     $io_select$
     $on_accessed$
     // ---- memory access

frameworks/libraries/riscv/ice-v/riscv-soc.si
@@ -44,9 +44,9 @@ $$end
 
   // io mapping
   always_before {
-    uint1 wen      <:  (memio.wenable != 0);
-    uint1 io_write <:  (memio.addr[$external$,1]    &  wen);
-    uint1 io_read  <:: (prev_mem_addr[$external$,1] & ~prev_mem_rw);
+    uint1 wen      = (memio.wenable != 0);
+    uint1 io_write = (memio.addr[$external$,1]    &  wen);
+    uint1 io_read  = (prev_mem_addr[$external$,1] & ~prev_mem_rw);
     $io_select$
     $on_accessed$
     // ---- memory access

frameworks/verilator/SPIScreen.h
@@ -99,4 +99,6 @@ public:
   LibSL::Image::ImageRGBA& framebuffer() override { return m_framebuffer; }
 
   bool framebufferChanged() override;
+  
+  bool ready() override { return true; }
 };

projects/easy-riscv/configs
@@ -1,0 +1,1 @@
+Makefile verilator

src/Algorithm.cpp
@@ -2904,6 +2904,10 @@ void Algorithm::gatherAlwaysAssigned(siliceParser::AlwaysAssignedListContext* al
 
 void Algorithm::checkPermissions(antlr4::tree::ParseTree *node, t_combinational_block *_current)
 {
+  const std::string notes =
+    "(Note : give permission using keywords reads/writes/readwrites/calls in parameter list\n"
+    "        e.g. 'subroutine test(reads a,calls alg) { ... }').\n";
+
   // gather info for checks
   std::unordered_set<std::string> all;
   std::unordered_set<std::string> read, written;
@@ -2917,12 +2921,16 @@ void Algorithm::checkPermissions(antlr4::tree::ParseTree *node, t_combinational_
   if (_current->context.subroutine != nullptr) {
     for (auto R : read) {
       if (_current->context.subroutine->allowed_reads.count(R) == 0) {
-        reportError(sourceloc(node), "variable '%s' is read by subroutine '%s' without explicit permission", R.c_str(), _current->context.subroutine->name.c_str());
+        std::string msg = "variable '%s' is read by subroutine '%s' without explicit permission\n\n";
+        msg += notes;
+        reportError(sourceloc(node), msg.c_str(), R.c_str(), _current->context.subroutine->name.c_str());
       }
     }
     for (auto W : written) {
       if (_current->context.subroutine->allowed_writes.count(W) == 0) {
-        reportError(sourceloc(node), "variable '%s' is written by subroutine '%s' without explicit permission", W.c_str(), _current->context.subroutine->name.c_str());
+        std::string msg = "variable '%s' is written by subroutine '%s' without explicit permission\n\n";
+        msg += notes;
+        reportError(sourceloc(node), msg.c_str(), W.c_str(), _current->context.subroutine->name.c_str());
       }
     }
   }
@@ -4332,7 +4340,6 @@ void Algorithm::updateAndCheckDependencies(t_vio_dependencies & _depds, const t_
       }
     }
   }
-
 }
 
 // -------------------------------------------------
@@ -4893,8 +4900,20 @@ void Algorithm::determineVIOAccess(
             }
           }
         }
-        // recurse
-        recurse = true;
+				// do not blindly recurse otherwise the child 'join' is reached
+        // NOTE: This is because the full instruction is "() <- called <- ()" so the left part (the join)
+        //       would be incorrectly considered and produce false dependencies.
+        //       The join is properly taken into account in the block that performs it (wait loop).
+				recurse = false;
+				// detect reads on parameters
+				for (auto c : node->children) {
+					if (dynamic_cast<siliceParser::JoinExecContext*>(c) != nullptr) {
+						// skip join, taken into account in return block
+						continue;
+
+					}
+					determineVIOAccess(c, vios, block, _read, _written);
+				}
       }
     } {
       auto async = dynamic_cast<siliceParser::AsyncExecContext*>(node);
@@ -6017,7 +6036,7 @@ void Algorithm::optimize(const t_instantiation_context& ictx)
     // determine type of temporaries
     determineTemporaries(ictx);
     // check var access permissions
-    checkPermissions();
+//    checkPermissions();
     // analyze variables access
     determineUsage();
   }

src/RISCVSynthesizer.cpp
@@ -329,8 +329,8 @@ string RISCVSynthesizer::generateSiliceCode(siliceParser::RiscvContext *riscv) c
     auto input  = dynamic_cast<siliceParser::InputContext *>   (io->input());
     auto output = dynamic_cast<siliceParser::OutputContext *>  (io->output());
     auto inout  = dynamic_cast<siliceParser::InoutContext *>   (io->inout());
-    io_select   = io_select + "uint1 ior_" + std::to_string(idx) + " <:: prev_mem_addr[" + std::to_string(idx) + ",1]; ";
-    io_select   = io_select + "uint1 iow_" + std::to_string(idx) + " <:  memio.addr[" + std::to_string(idx) + ",1]; ";
+    io_select   = io_select + "uint1 ior_" + std::to_string(idx) + " = prev_mem_addr[" + std::to_string(idx) + ",1]; ";
+    io_select   = io_select + "uint1 iow_" + std::to_string(idx) + " = memio.addr[" + std::to_string(idx) + ",1]; ";
     string v, init;
     t_type_nfo type_nfo;
     if (input) {

tests/alg_call_grp.si
@@ -18,6 +18,5 @@ algorithm main(output uint8 leds)
   test t;
 
   foo.value = 13;
-++:
   (foo,leds) <- t <- (foo);
 }

tests/algtm3.si
@@ -31,9 +31,6 @@ algorithm main(output uint8 leds)
 
     __display("[cycle %d] (main) calling",cycle);
     g.b = cycle;
- ++: // this is required due to a limitation: Silice pessimistically considers
-    // all group members to be read and written, hence producing a false
-    // positive on a cyclic dependency
     (g) <- alg_inst <- (g);
     g.b = g.a + 1;
     __display("[cycle %d] (main) done, a = %d",cycle,g.a);

tests/error_checks/freedecl2.si
@@ -25,7 +25,6 @@ algorithm main(output uint8 leds)
   uint8 a = 0;
 
   a = 2;
-  b = 3;
 
   {
     uint8 c = 1;

tests/error_checks/piptrack.si
@@ -10,7 +10,7 @@ unit main(output uint8 leds)
             a = counter;
             __display("[0] a = %d",a);
         } -> {
-            sameas(a) t <: a;
+            sameas(a) t <: a; // error, not allowed here
             __display("[1] t = %d",t);
         } -> {
             __display("[2] a = %d",a);

tests/freedecl1.si
@@ -6,8 +6,6 @@ algorithm main(output uint8 leds)
 
     v = 3;
 
-    e = 1;
-
     {
       uint4 e = 10;
       v = e + 1;
@@ -18,8 +16,7 @@ algorithm main(output uint8 leds)
   uint8 a = 0;
 
   a = 2;
-  b = 3;
-
+  
   {
     uint8 c = 1;
     uint8 b = 1;
@@ -27,6 +24,7 @@ algorithm main(output uint8 leds)
     a = b + 1;
   }
 
+  uint8 b = 1;
   (b) <- foo <- ();
 
 }

tests/group2.si
@@ -23,3 +23,5 @@ algorithm alg(
   (t0) = foo(t0);
 
 }
+
+unit main(output uint8 leds) {}

tests/if2.si
@@ -9,9 +9,10 @@ algorithm main(output uint8 leds)
   b = a+1;
 
   if (data == 0) {
-    a = 1;
     if (data == 5) {
-      a = a*2+1;
+      a = 5;
+    } else {
+      a = 1;
     }
     if (b == 0) {
       a = 3;

tests/noname1.si
@@ -11,6 +11,6 @@ algorithm main(output int8 leds)
   uint4 w <: 0;
   test _(a <: w,  b :> leds, c <: w);
   uint4 w2 <: 5;
-  sameas(leds) tmp;
+  sameas(leds) tmp=0;
   test _(a <: w2, b :> tmp, c <: w);
 }

tests/onehot1.si
@@ -3,12 +3,12 @@ algorithm main(
   output uint8 leds
 ) <onehot>
 {
-  led = 0;
+  leds = 0;
 ++:
-  led = 1;
+  leds = 1;
 ++:
-  led = 2;
+  leds = 2;
 ++:
-  led = 3;
+  leds = 3;
 
 }

tests/preproc1.si
@@ -8,3 +8,5 @@ $$ for i=0,3 do
 $$end
 
 }
+
+unit main(output uint8 leds) {}

tests/preproc10.si
@@ -1,4 +1,4 @@
-unit mirror(input auto i,output uint$widthof('i')$ o)
+unit mirror(input auto i,output sameas(i) o)
 {
   always {
 $$for n=0,widthof('i')-1 do

tests/preproc3.si
@@ -16,10 +16,10 @@ $$ if w_in == 2 then
    out = ~in[1,1];
 $$ else
    uint$clog2(w_in)-1$ half_count = uninitialized;
-   uint$w_h$           lhs        <: in[$w_h$,$w_h$];
-   uint$w_h$           rhs        <: in[    0,$w_h$];
-   uint$w_h$           select     <: left_empty ? rhs : lhs;
-   uint1               left_empty <: ~|lhs;
+   uint$w_h$           lhs        = in[$w_h$,$w_h$];
+   uint$w_h$           rhs        = in[    0,$w_h$];
+   uint$w_h$           select     = left_empty ? rhs : lhs;
+   uint1               left_empty = ~|lhs;
    (half_count) = $name$_$w_h$(select);
    out          = {left_empty,half_count};
 $$ end

tests/rep1.si
@@ -3,7 +3,7 @@ algorithm main(output int8 leds)
   int8 tbl[8] = {1,2,4,8,16,32,64,128};
   int8 myled = 0;
 
-  led := myled;
+  leds := myled;
 
   8x {
     tbl[__id] = __id;

tests/riscv1.si
@@ -1,12 +1,12 @@
 
-riscv cpu_test(input uint32 a,output uint32 b) <mem=512> = compile({
+riscv cpu_test(input uint32 a,output uint32 b) <mem=512> {
 
-  int f(int val) 
+  int f(int val)
   {
     return val + 1;
   }
 
-  void main() { 
+  void main() {
     int i=0;
     while (1) {
       // b(f(a()));
@@ -14,20 +14,20 @@ riscv cpu_test(input uint32 a,output uint32 b) <mem=512> = compile({
       ++i;
     }
   }
-  
-})
+
+}
 
 algorithm main(output uint8 leds)
 {
   uint8 iter = 0;
   cpu_test cpu0;
-  
+
   cpu0.a := iter;
   leds   := cpu0.b;
-  
+
   while (iter != 255) {
     __display("LEDs = %x",leds);
     iter = iter + 1;
   }
-  
+
 }

tests/sameas3.si
@@ -1,6 +1,6 @@
 algorithm main(output uint8 leds)
 {
-  sameas(leds) a;
+  sameas(leds) a(0);
   
   leds = a + 1;
 }

tests/sameas4.si
@@ -14,7 +14,7 @@ algorithm test(
   i_g    it,
   output uint8 v
 ) {
-  sameas(it.value) tmp1;
+  sameas(it.value) tmp1(0);
   tmp1        = widthof(it.value) + 3;
   v           = tmp1;
 }

tests/sub2.si
@@ -3,15 +3,15 @@ algorithm passthrough(input uint8 i,output uint8 o)
   o = i;
 }
 
-algorithm main(output uint8 led)
-{ 
+algorithm main(output uint8 leds)
+{
   int8 a = 0;
   int8 b = 0;
   int8 c = 0;
 
   passthrough p0;
 
-  subroutine test(reads a) {
+  subroutine test(reads a,calls p0) {
 	  p0.i = a + 2;
     return;
   }

tests/subchain1.si
@@ -1,23 +1,14 @@
-algorithm main(output uint8 led)
+algorithm main(output uint8 leds)
 {
 
   subroutine inc(input uint8 a,output uint8 r)
   {
-    uint8 tmp = 0;
-    tmp = a + 1;
-    r   = tmp;
+    r = a;
   }
 
   subroutine foo(input uint8 a,output uint8 r)
   {
-    uint8 tmp1 = 3;
-    uint8 tmp2 = 0;
-    tmp2 = a;
-    while (tmp1 > 0) {
-      tmp1 = tmp1 - 1;
-      tmp2 = tmp2 + a;
-    }
-    r = tmp2;
+    r = a;
   }
 
   uint8 v = 0;

tests/tbl2.si
@@ -1,42 +1,0 @@
-algorithm test(input int8 tbl[8],output int8 v)
-{
-  v = tbl[0] | tbl[1] | tbl[2] | tbl[3] 
-    | tbl[4] | tbl[5] | tbl[6] | tbl[7];
-}
-
-algorithm wait()
-{
-  int22 w = 0;
-loop:
-  w = w + 1;
-  if (w != 0) { goto loop; }
-}
-
-algorithm main(output int8 led)
-{
-  wait w;
-  test tst;
-  int8 tbl[8] = {1,2,4,8,16,32,64,128};
-  int8 myled = 0;
-  int8 i = 0;
-  
-  led := myled;
-
-loop:
-
-   i = 0;
-   
-fori:
-  myled = tbl[i];
-  //() <- w <- ();
-  i = i + 1;
-  if (i < 8) {
-    goto fori;
-  }
-  
-  (myled) <- tst <- (tbl);
-  //() <- w <- ();
-  
-  goto loop;
-  
-}

tests/tbl3.si
@@ -1,33 +1,0 @@
-algorithm test(input int8 tbl[4],output int8 v[4])
-{
-  4x {
-    v[__id] = tbl[3-__id];
-  }
-}
-
-algorithm wait()
-{
-  int22 w = 0;
-loop:
-  w = w + 1;
-  if (w != 0) { goto loop; }
-}
-
-algorithm main(output int8 led)
-{
-  wait w;
-  test t;
-  int8 tbl[4] = {21,42,84,168};
-  int8 myled = 0;
-  
-  led := myled;
-  
-loop:
-  
-  (tbl) <- t <- (tbl);
-  myled = tbl[0];
-  () <- w <- ();
-  
-  goto loop;
-  
-}

tests/test_all.sh
@@ -4,6 +4,7 @@ tests=$(find -maxdepth 1 -name '*.si' -printf '%f\n' | sed 's/\.si$//1' | sort -
 
 task() {
   mkcmd="make $1 ARGS=\"--no_build\"";
+  # mkcmd="make $1";
   cmd_out=`eval $mkcmd 2>&1`
   has_err=$(echo $cmd_out | grep -i "error");
   if [ -z "$has_err" ];

tests/vars1.si
@@ -1,88 +1,0 @@
-import('uart_rx.v')
-import('uart_tx.v')
-import('spi_slave.v')
-import('cclk_detector.v')
-import('avr_interface.v')
-
-algorithm main(
-  input  int1 cclk,
-  input  int1 spi_ss,
-  input  int1 spi_sck,
-  input  int1 avr_tx,
-  input  int1 avr_rx_busy,
-  input  int1 spi_mosi,
-  output int1 spi_miso,
-  output int1 avr_rx,
-  output int4 spi_channel,
-  output int8 led,
-  output int8 d1_c,
-  output int8 d1_r,
-  output int8 d1_g,
-  output int8 d1_b,
-  output int8 d2_c,
-  output int8 d2_r,
-  output int8 d2_g,
-  output int8 d2_b
-  )
-{
-  // LEDs
-  int8  col   = 0;
-  int16 md_r  = 16hffff;
-  int16 md_g  = 16hffff;
-  int16 md_b  = 16hffff;
-  int18 count = 0;
-  int8  tmp   = 0;
-  int8  wonly = 0;
-  
-  // UART
-  int4  channel = 4hf;
-  int8  tx_data = 0;
-  int8  rx_data = 0;
-  int1  new_tx_data = 0;
-  int1  new_rx_data = 0;
-  
-  // avr module
-  avr_interface avr(
-    rst         <: reset,
-    rx          <: avr_tx,
-    tx_block    <: avr_rx_busy,
-    tx          :> avr_rx,
-    <:auto:>
-  );
-  
-  d1_c := ~col;
-  d2_c := ~col;
-  d1_r := md_r[0,8];
-  d1_g := md_g[0,8];
-  d1_b := md_b[0,8];
-  d2_r := md_r[8,8];
-  d2_g := md_g[8,8];
-  d2_b := md_b[8,8];
-  led  := 0;
-
-  // echo
-  tx_data     := rx_data;
-  new_tx_data := new_rx_data;
-
-scanline:
-  
-  col   = 0;
-  wonly = 1;
-  
-++:
-  
-  count = count + 1;
-  col   = (1 << count[10,3]);
-
-++:
-  
-  tmp = 1;
-  wonly = tmp;
-  md_r[3] = tmp;
-  
-++:
-
-  tmp = 3 + col;
-  
-  goto scanline;
-}

tests/widthof1.si
@@ -1,6 +1,6 @@
 algorithm main(output uint8 leds)
 {
-  sameas(leds) a;
-  
+  sameas(leds) a(0);
+
   leds = a + widthof(leds);
 }