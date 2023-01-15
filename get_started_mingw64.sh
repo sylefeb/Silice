cd5efc79e5f25fef0e6dcea8e3c8ae4707f5e8c9
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2023-01-15 17:11:53 +0100

    update to latest fpga-binutils and additional packages in mingw install

--- get_started_mingw64.sh
+++ get_started_mingw64.sh
@@ -22,11 +22,13 @@ pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-iverilog
 pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-gtkwave
 pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-verilator
 pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-openFPGALoader
+pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-dfu-util
 # pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-nextpnr
 # pacman -S --noconfirm --needed  ${MINGW_PACKAGE_PREFIX}-icestorm
 # pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-prjtrellis
+pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-boost
 
-wget -c https://github.com/sylefeb/fpga-binutils/releases/download/v20220607/fpga-binutils-64.zip
+wget -c https://github.com/sylefeb/fpga-binutils/releases/download/v20230115/fpga-binutils-64.zip
 
 unzip -o fpga-binutils-64.zip -d tools/fpga-binutils/
 
