f5efc89b630b9a0a654da1a8a93c59b9fc6f2a07
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2023-08-17 18:59:33 +0200

    swirl muldiv, stall wip

--- projects/ice-v/compile/icebreaker/swirl/compile_c.sh
+++ projects/ice-v/compile/icebreaker/swirl/compile_c.sh
@@ -12,10 +12,10 @@ echo "using $ARCH"
 BASE=./compile/icebreaker/swirl
 DST=./compile/build
 
-$ARCH-gcc -DICEBREAKER_SWIRL -fstack-reuse=none -fno-builtin -O3 -fno-stack-protector -fno-pic -march=rv32i -mabi=ilp32 -T$BASE/config_c.ld -ffreestanding -nostdlib -o $DST/code.elf $BASE/crt0.s $1
+$ARCH-gcc -DICEBREAKER_SWIRL -fstack-reuse=none -fno-builtin -O3 -fno-stack-protector -fno-pic -march=rv32im -mabi=ilp32 -T$BASE/config_c.ld -ffreestanding -nostdlib -o $DST/code.elf $BASE/crt0.s $1
 
 $ARCH-objcopy -O verilog $DST/code.elf $DST/code.hex
 
 # uncomment to see the actual code, useful for debugging
-# $ARCH-objcopy.exe -O binary $DST/code.elf $DST/code.bin
-# $ARCH-objdump.exe -D -m riscv $DST/code.elf
+$ARCH-objcopy.exe -O binary $DST/code.elf $DST/code.bin
+$ARCH-objdump.exe -D -m riscv $DST/code.elf > $DST/code.s
