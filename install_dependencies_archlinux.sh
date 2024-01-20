82b855595354a99d46f4b35474302738b656a1d4
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2024-01-20 21:19:28 +0100

    adding riscv packages

--- install_dependencies_archlinux.sh
+++ install_dependencies_archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S jre-openjdk jdk-openjdk git gcc make cmake pkg-config glfw freeglut
+pacman -S jre-openjdk jdk-openjdk git gcc make cmake pkg-config glfw freeglut riscv64-linux-gnu-gcc
