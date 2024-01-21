e3df3af448fc99c3c3bb3ffb38aa6ef45b16d4f9
Sylvain Lefebvre <Sylvain.Lefebvre@inria.fr>
2024-01-21 18:10:16 +0100

    removing freeglut3 from dependencies

--- install_dependencies_archlinux.sh
+++ install_dependencies_archlinux.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-pacman -S jre-openjdk jdk-openjdk git gcc make cmake pkg-config glfw freeglut riscv64-linux-gnu-gcc
+pacman -S jre-openjdk jdk-openjdk git gcc make cmake pkg-config glfw riscv64-linux-gnu-gcc
