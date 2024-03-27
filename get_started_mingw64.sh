e729cd7ef47ac5728f6126d433957efe76ce14db
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-03-27 17:29:04 +0100

    fix for latest oss-cad-suite packages

--- get_started_mingw64.sh
+++ get_started_mingw64.sh
@@ -29,7 +29,7 @@ pacman -S --noconfirm --needed ${MINGW_PACKAGE_PREFIX}-glfw
 
 # -------------- retrieve oss-cad-suite package --------------
 OSS_CAD_MONTH=03
-OSS_CAD_DAY=12
+OSS_CAD_DAY=27
 OSS_CAD_YEAR=2024
 OSS_PACKAGE=oss-cad-suite-windows-x64-$OSS_CAD_YEAR$OSS_CAD_MONTH$OSS_CAD_DAY.exe
 
