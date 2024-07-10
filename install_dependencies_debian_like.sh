49f6feeec55b1d014c8b373153f77575e222d780
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-07-10 18:26:48 +0200

    adjustment to linux install scripts

--- install_dependencies_debian_like.sh
+++ install_dependencies_debian_like.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sudo apt install default-jre default-jdk git gcc g++ make cmake pkg-config uuid uuid-dev libglfw3 libglfw3-dev gcc-riscv64-linux-gnu
+sudo apt install default-jre default-jdk git gcc g++ make cmake pkg-config uuid uuid-dev libglfw3 libglfw3-dev gcc-riscv64-linux-gnu python3
+
