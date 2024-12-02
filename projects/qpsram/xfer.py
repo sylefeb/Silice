db9c28b3a9e4e1e70f071399ff9e64bda234e5f1
Sylvain Lefebvre <sylvain.lefebvre@inria.fr>
2024-12-02 20:22:42 +0100

    wip: musbx, qpsram, framework

--- projects/qpsram/xfer.py
+++ projects/qpsram/xfer.py
@@ -11,8 +11,8 @@ if len(sys.argv) < 4:
   sys.exit()
 
 # open serial port
-ser = serial.Serial(sys.argv[1],500000, timeout=1)
-# ser = serial.Serial(sys.argv[1],115200, timeout=1)
+# ser = serial.Serial(sys.argv[1],500000, timeout=1)
+ser = serial.Serial(sys.argv[1],115200, timeout=1)
 
 # op to perform
 op = sys.argv[2]
